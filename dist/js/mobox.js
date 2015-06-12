0485b8f1defdba9a427e499b9869a4d0a652c32b
mamboer <mamboer@live.com>
2015-06-12 17:34:17 +0800

    gogo

--- dist/js/mobox.js
+++ dist/js/mobox.js
@@ -6,7 +6,7 @@
  *  2. modernizr
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * built at 1434080092574 
+ * built at 1434082760247 
  * Copyright 2015, FASO.ME <http://www.faso.me>
  */
 (function (root, factory) {
