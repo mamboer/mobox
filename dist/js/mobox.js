bc825998daff209e3ca817c341a8036f388895f5
mamboer <mamboer@live.com>
2015-06-14 17:38:24 +0800

    fix demo

--- dist/js/mobox.js
+++ dist/js/mobox.js
@@ -6,7 +6,7 @@
  *  2. modernizr
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * built at 1434082760247 
+ * built at 1434268184016 
  * Copyright 2015, FASO.ME <http://www.faso.me>
  */
 (function (root, factory) {
